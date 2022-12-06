0bb5bbdaf02935df7806bca3e8c252de86d65146
David Knapik <david.knapik@gmail.com>
2022-12-06 09:48:22 -0700

    day02 part 2 implemented

--- day02/day02.pl
+++ day02/day02.pl
@@ -3,10 +3,18 @@
 #########################################
 #    Opponent    #    Player      # Pts #
 #########################################
-#  A = Rock      #  X = Rock      #  1  #
-#  B = Paper     #  Y = Paper     #  2  #
-#  C = Scissors  #  Z = Scissors  #  3  #
+#  A (Rock)      #  X (Rock)      #  1  #
+#  B (Paper)     #  Y (Paper)     #  2  #
+#  C (Scissors)  #  Z (Scissors)  #  3  #
 #########################################
+
+########################
+#        Part 1        #
+########################
+#  X (Rock)      #  1  #
+#  Y (Paper)     #  2  #
+#  Z (Scissors)  #  3  #
+########################
 
 ##################
 # Outcome Points #
@@ -16,8 +24,51 @@
 #   Win    #  6  #
 ##################
 
+#######################################
+#               Part 1                #
+#             Score Table             #
+#######################################
+# A (rock)     X (rock)     # 1+3 # 4 #
+# A (rock)     Y (paper)    # 2+6 # 8 #
+# A (rock)     Z (scissors) # 3+0 # 3 #
+#######################################
+# B (paper)    X (rock)     # 1+0 # 1 #
+# B (paper)    Y (paper)    # 2+3 # 5 #
+# B (paper)    Z (scissors) # 3+6 # 9 #
+#######################################
+# C (scissors) X (rock)     # 1+6 # 7 #
+# C (scissors) Y (paper)    # 2+0 # 2 #
+# C (scissors) Z (scissors) # 3+3 # 6 #
+#######################################
+
+####################
+#      Part 2      #
+####################
+#  X (Lose)  #  0  #
+#  Y (Draw)  #  3  #
+#  Z (Win)   #  6  #
+####################
+
+#######################################
+#               Part 2                #
+#            Score Table              #
+#######################################
+# A (rock)     X (scissors) # 3+0 # 3 #
+# A (rock)     Y (rock)     # 1+3 # 4 #
+# A (rock)     Z (paper)    # 2+6 # 8 #
+#######################################
+# B (paper)    X (rock)     # 1+0 # 1 #
+# B (paper)    Y (paper)    # 2+3 # 5 #
+# B (paper)    Z (scissors) # 3+6 # 9 #
+#######################################
+# C (scissors) X (paper)    # 2+0 # 2 #
+# C (scissors) Y (scissors) # 3+3 # 6 #
+# C (scissors) Z (rock)     # 1+6 # 7 #
+#######################################
+
 $input_file = 'input.txt' ;
-$total_score = 0 ;
+$total_score_1 = 0 ;
+$total_score_2 = 0 ;
 
 # read in input file
 open(FH, '<', $input_file) 
@@ -28,35 +79,35 @@ while(<FH>) {
 	chomp($line = $_) ;
 	printf("Line: %s\n", $line);
 
-#################
-#  Score Table  #
-#################
-# A X # 1+3 # 4 #
-# A Y # 2+6 # 8 #
-# A Z # 3+0 # 3 #
-#################
-# B X # 1+0 # 1 #
-# B Y # 2+3 # 5 #
-# B Z # 3+6 # 9 #
-#################
-# C X # 1+6 # 7 #
-# C Y # 2+0 # 2 #
-# C Z # 3+3 # 6 #
-#################
+	# totals for Part 1
+	($line eq "A X") ? ($total_score_1 += 4) : ($total_score_1) ;
+	($line eq "A Y") ? ($total_score_1 += 8) : ($total_score_1) ;
+	($line eq "A Z") ? ($total_score_1 += 3) : ($total_score_1) ;
 
-	($line eq "A X") ? ($total_score += 4) : ($total_score) ;
-	($line eq "A Y") ? ($total_score += 8) : ($total_score) ;
-	($line eq "A Z") ? ($total_score += 3) : ($total_score) ;
+	($line eq "B X") ? ($total_score_1 += 1) : ($total_score_1) ;
+	($line eq "B Y") ? ($total_score_1 += 5) : ($total_score_1) ;
+	($line eq "B Z") ? ($total_score_1 += 9) : ($total_score_1) ;
 
-	($line eq "B X") ? ($total_score += 1) : ($total_score) ;
-	($line eq "B Y") ? ($total_score += 5) : ($total_score) ;
-	($line eq "B Z") ? ($total_score += 9) : ($total_score) ;
+	($line eq "C X") ? ($total_score_1 += 7) : ($total_score_1) ;
+	($line eq "C Y") ? ($total_score_1 += 2) : ($total_score_1) ;
+	($line eq "C Z") ? ($total_score_1 += 6) : ($total_score_1) ;
 
-	($line eq "C X") ? ($total_score += 7) : ($total_score) ;
-	($line eq "C Y") ? ($total_score += 2) : ($total_score) ;
-	($line eq "C Z") ? ($total_score += 6) : ($total_score) ;
+	# totals for Part 2
+	($line eq "A X") ? ($total_score_2 += 3) : ($total_score_2) ;
+	($line eq "A Y") ? ($total_score_2 += 4) : ($total_score_2) ;
+	($line eq "A Z") ? ($total_score_2 += 8) : ($total_score_2) ;
+
+	($line eq "B X") ? ($total_score_2 += 1) : ($total_score_2) ;
+	($line eq "B Y") ? ($total_score_2 += 5) : ($total_score_2) ;
+	($line eq "B Z") ? ($total_score_2 += 9) : ($total_score_2) ;
+
+	($line eq "C X") ? ($total_score_2 += 2) : ($total_score_2) ;
+	($line eq "C Y") ? ($total_score_2 += 6) : ($total_score_2) ;
+	($line eq "C Z") ? ($total_score_2 += 7) : ($total_score_2) ;
+
 
 }
 
 # output total score
-printf("Total: %s\n", $total_score) ;
+printf("Total (part 1): %s\n", $total_score_1) ;
+printf("Total (part 2): %s\n", $total_score_2) ;
